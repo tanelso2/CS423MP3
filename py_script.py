66fe69673ce72fc6afa707da91907e0260786625
CS423 Group 06 <tanelso2@illinois.edu>
2016-04-05 19:33:13 -0500

    commiting graph and py_script

--- py_script.py
+++ py_script.py
@@ -14,17 +14,21 @@ data = []
 for line in lines:
 	data.append(line.split(' '))
 
-del data[-1]  
+del data[-1] 
+accum_value = 0
+page_fault_accum = []
 for entry in data:
 	entry[0] = int(entry[0])
  	entry[1] = int(entry[1])
  	entry[2] = int(entry[2])
  	entry[3] = int(entry[3].rstrip())
- 
+	accum_value += entry[0] + entry[1]
+	page_fault_accum.append(accum_value)	
+
 data_np = np.array(data)
 print data_np
 
 
-plt.plot(data_np[:,3], data_np[:,2])
+plt.plot(data_np[:,3], page_fault_accum)
 plt.savefig("profile1_graph.png")
 
